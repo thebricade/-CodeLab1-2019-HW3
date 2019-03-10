4d853d4d96ab9939b724374440265fc7773c3c33
Bri <bs3864@nyu.edu>
2019-03-09 20:45:02 -0500

    Making Dino Move on their own

---
+++ CodeLab-Week3/Assets/Scripts/DinoControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //roomba should  move forward 2 meters per second
+        transform.Translate(2f*-Time.deltaTime,0f,0f);
+        
+        
+        
+        //make a ray shooting out of the front of the roomba
+        Ray myRay = new Ray(transform.position, transform.forward);
+        
+        //2 mdefine the max raycast distance
+        float maxRayDist = 2f;
+        
+        //3 debug
+        Debug.DrawRay(myRay.origin,myRay.direction *maxRayDist, Color.cyan);
+
+        if (Physics.Raycast(myRay, maxRayDist))
+        {
+            int randomNumber = Random.Range(0, 100);
+            if (randomNumber >= 50)
+            {
+                transform.Rotate(0f,-90f,0f);
+            }
+            else
+            {
+                transform.Rotate(0f,90f,0f);
+            }
+        }
+        
+    }
+}
---
+++ CodeLab-Week3/Assets/Scripts/DinoGround.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoGround : MonoBehaviour
+{
+    public bool isGrounded;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //step 1 for raycasting make/declare a ray object
+        Ray ray = new Ray(transform.position, Vector3.down);
+        
+        //step 2 speccify maxium raycast distance 
+        float raycastDist = 0.6f;
+        
+        //set 3 debug draw the raycast
+        Debug.DrawRay(ray.origin, ray.direction, Color.yellow);
+        
+        //set 4 
+        if (Physics.Raycast(ray, raycastDist))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        
+        //sphere cast is more commonly used and boxcast 
+    }
+ }
+
