fb51ed7d9b247d191245ed0659e6a7d56f2ef929
Bri <bs3864@nyu.edu>
2019-03-10 03:05:37 -0400

    Menu Screen bug - dino disappears and the moving and hitting wall code is shaky

--- CodeLab-Week3/Assets/Scripts/GameManager.cs
+++ CodeLab-Week3/Assets/Scripts/GameManager.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameState
+{
+    title,
+    pause,
+    playing
+}
 
 public class GameManager : MonoBehaviour
 {
     int score = 0;
+
+    private GameState currentState;
+    private GameObject pauseMenu;
+    private GameObject[] titleScreenObjects;
+    
+    //Title Screen
+    public Button playButton;
 
     public int Score
     {
@@ -29,6 +44,13 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //setting Game Menu objects
+        currentState = GameState.title;
+        //setupButton
+        GameObject titleMenu = Instantiate((Resources.Load<GameObject>("Prefabs/TitleMenu")));
+        playButton = GameObject.Find("Play").GetComponent<Button>();
+        playButton.onClick.AddListener(TaskOnClick);
+        
         //Singleton (there can only be one) 
         if (instance == null)
         {
@@ -45,6 +67,55 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         Score += 7;
+        checkGameState();
+
+    }
+
+    void checkGameState()
+    {
+        switch (currentState)
+        {
+            case GameState.pause:
+                //When the Gamestate is PAUSED set the timeScale to 0  to stop everything from spawning and moving
+                if (Time.timeScale > 0f)
+                {
+                    GameObject pauseMenu = Instantiate(Resources.Load<GameObject>("Prefabs/PauseMenu"));
+                    Time.timeScale = 0f;
+                }
+                
+                //wWhen you press SPACE in a PAUSE state you should go back to PLAYING
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Destroy(GameObject.FindWithTag("Pause"));
+                    currentState = GameState.playing;
+                }
+                break;
+            case GameState.playing:
+                Time.timeScale = 1.0f;
+                
+                //While in PLAYING state if the SPACE is pressed
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    currentState = GameState.pause;
+                }
+                break;
+            case GameState.title:
+                Time.timeScale = 0f;
+                break;
+        }
+    }
+
+    void TaskOnClick()
+    {
         
+        //Get all objects in Title State and put them in an array to destroy
+        titleScreenObjects = GameObject.FindGameObjectsWithTag("TitleScreen");
+
+        for (var i = 0; i < titleScreenObjects.Length; i++)
+        {
+            Destroy(titleScreenObjects[i]);
+        }
+        
+        currentState = GameState.playing;
     }
 }
